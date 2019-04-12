36a2c954885f82df0fa4bd5bf0b1e9fd6ec42ded
MonPrénom MonNom <me@mydomain.org>
2019-04-12 12:28:38 +0200

    Revert "Finalement changement de liba_byebye en liba_goodbye"
    This reverts commit 7d5cbac52fa3cf41616a05dee283c5db6a99b4f7.

--- app/src/app.c
+++ app/src/app.c
@@ -15,14 +15,6 @@ int main(int argc, char **argv) {
   libb_hello();
   fprintf(stdout, "%s", ":: calling libb_hello()... [done]\n");
 
-
-  fprintf(stdout, "%s", ":: calling liba_goodbye()...\n");
-  liba_goodbye();
-  fprintf(stdout, "%s", ":: calling liba_goodbye()... [done]\n");
-
-
-
-
   fprintf(stdout, "%s", ":: bye.\n");
 
   return 0;
